cdb4e964d7d9c3c3c7b7b56055cdb5f4c31d567b
Rodrigo Bastos <rodrigobastosv@gmail.com>
2021-11-24 09:45:46 -0300

    reverting main sample

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:fancy_password_field/fancy_password_field.dart';
 import 'package:flutter/material.dart';
 
 import 'samples/samples.dart';
@@ -22,70 +21,9 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: const [
             /// Change this to one of the available samples on the samples folder
-            //Sample1(),
-            Center(
-              child: SizedBox(
-              width: 400,
-              child: FancyPasswordField(
-                  validationRules: {
-                      DigitValidationRule(),
-                      UppercaseValidationRule(),
-                      LowercaseValidationRule(),
-                      SpecialCharacterValidationRule(),
-                      MinCharactersValidationRule(6),
-                      MaxCharactersValidationRule(12),
-                  },
-                  strengthIndicatorBuilder: (strength) => Text(
-                    strength.toString(),
-                  ),
-                  validationRuleBuilder: (rules, value) {
-                    if (value.isEmpty) {
-                      return const SizedBox.shrink();
-                    }
-                    return ListView(
-                      shrinkWrap: true,
-                      children: rules
-                          .map(
-                            (rule) => rule.validate(value)
-                                ? Row(
-                                    children: [
-                                      const Icon(
-                                          Icons.check,
-                                          color: Colors.green,
-                                      ),
-                                      const SizedBox(width: 12),
-                                      Text(
-                                          rule.name,
-                                          style: const TextStyle(
-                                              color: Colors.green,
-                                          ),
-                                      ),
-                                    ],
-                                  )
-                                : Row(
-                                    children: [
-                                      const Icon(
-                                          Icons.close,
-                                          color: Colors.red,
-                                      ),
-                                      const SizedBox(width: 12),
-                                      Text(
-                                          rule.name,
-                                          style: const TextStyle(
-                                              color: Colors.red,
-                                          ),
-                                      ),
-                                    ],
-                                  ),
-                          )
-                          .toList(),
-                    );
-                  },
-                ),
-              ),
-            ),
+            Sample1(),
           ],
         ),
       ),
