5d68c5a6759c545557ebc3acc9f176c45c3ffef3
Matthieu Rosset <matthieu.rosset@unifr.ch>
2022-10-13 15:03:53 +0200

    add customText

--- lib/src/validation_rule.dart
+++ lib/src/validation_rule.dart
@@ -1,43 +1,13 @@
-/// Base class representing a validation rule
-/// Every new validation should extends this one.
-///
-abstract class ValidationRule {
-  /// Name of the rule. You can use this atribute to
-  /// show the name of the rule to the user, for example.
-  String get name;
-
-  /// A function that should be implemented and it represents
-  /// the validation of the value. It should return [Boolean] indicating
-  /// that the value passed pass the validation or not.
-  bool validate(String value);
-}
-
-/// Validates that the value has at least one uppercase letter
-class UppercaseValidationRule extends ValidationRule {
-  @override
-  String get name => 'Has uppercase letter';
-
-  @override
-  bool validate(String value) {
-    return value.contains(RegExp(r'[A-Z]'));
-  }
-}
-
-/// Validates that the value has at least one lowercase letter
-class LowercaseValidationRule extends ValidationRule {
-  @override
-  String get name => 'Has lowercase letter';
-
-  @override
-  bool validate(String value) {
-    return value.contains(RegExp(r'[a-z]'));
-  }
-}
-
 /// Validates that the value has at least one digit
 class DigitValidationRule extends ValidationRule {
+  final String? _customText;
+
+  DigitValidationRule({
+    String? customText,
+  }) : _customText = customText;
+
   @override
-  String get name => 'Has digit';
+  String get name => _customText ?? 'Has digit';
 
   @override
   bool validate(String value) {
@@ -45,33 +15,20 @@ class DigitValidationRule extends ValidationRule {
   }
 }
 
-/// Validates that the value has at least one special character
-class SpecialCharacterValidationRule extends ValidationRule {
-  @override
-  String get name => 'Has special character';
+/// Validates that the value has at least one lowercase letter
+class LowercaseValidationRule extends ValidationRule {
+  final String? _customText;
+
+  LowercaseValidationRule({
+    String? customText,
+  }) : _customText = customText;
 
   @override
-  bool validate(String value) {
-    return value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
-  }
-}
-
-/// Validates that the value has at least [numberOfCharacters]
-///
-/// Throws [AssertionError] if numberOfCharacters <= 0.
-class MinCharactersValidationRule extends ValidationRule {
-  MinCharactersValidationRule(this._numberOfCharacters)
-      : assert(_numberOfCharacters > 0,
-            'numberOfCharacters must be greater than 0');
-
-  final int _numberOfCharacters;
-
-  @override
-  String get name => 'Min of $_numberOfCharacters characters';
+  String get name => _customText ?? 'Has lowercase letter';
 
   @override
   bool validate(String value) {
-    return value.length >= _numberOfCharacters;
+    return value.contains(RegExp(r'[a-z]'));
   }
 }
 
@@ -79,14 +36,20 @@ class MinCharactersValidationRule extends ValidationRule {
 ///
 /// Throws [AssertionError] if numberOfCharacters <= 0.
 class MaxCharactersValidationRule extends ValidationRule {
-  MaxCharactersValidationRule(this._numberOfCharacters)
-      : assert(_numberOfCharacters > 0,
-            'numberOfCharacters must be greater than 0');
-
+  final String? _customText;
   final int _numberOfCharacters;
 
+  MaxCharactersValidationRule(
+    this._numberOfCharacters, {
+    String? customText,
+  })  : assert(_numberOfCharacters > 0,
+            'numberOfCharacters must be greater than 0'),
+        _customText = customText;
+
   @override
-  String get name => 'Max of $_numberOfCharacters characters';
+  String get name =>
+      _customText?.replaceAll('{1}', _numberOfCharacters.toString()) ??
+      'Max of $_numberOfCharacters characters';
 
   @override
   bool validate(String value) {
@@ -100,24 +63,103 @@ class MaxCharactersValidationRule extends ValidationRule {
 /// Throws [AssertionError] if max <= 0.
 /// Throws [AssertionError] if min > max.
 class MinAndMaxCharactersValidationRule extends ValidationRule {
+  final String? _customText;
+  final int _min;
+  final int _max;
   MinAndMaxCharactersValidationRule({
     required int min,
     required int max,
+    String? customText,
   })  : assert(min > 0, 'min must be greater than 0'),
         assert(max > 0, 'max must be greater than 0'),
         assert(max >= min, 'max must be greater than min'),
         _min = min,
-        _max = max;
-
-  final int _min;
-  final int _max;
+        _max = max,
+        _customText = customText;
 
   @override
-  String get name => 'Min $_min and Max $_max characters';
+  String get name =>
+      _customText
+          ?.replaceAll('{1}', _min.toString())
+          .replaceAll('{2}', _max.toString()) ??
+      'Min $_min and Max $_max characters';
 
   @override
   bool validate(String value) {
     return (value.length >= _min && value.length < _max) ||
         (value.length > _min && value.length <= _max);
   }
+}
+
+/// Validates that the value has at least [numberOfCharacters]
+///
+/// Throws [AssertionError] if numberOfCharacters <= 0.
+class MinCharactersValidationRule extends ValidationRule {
+  final String? _customText;
+  final int _numberOfCharacters;
+
+  MinCharactersValidationRule(
+    this._numberOfCharacters, {
+    String? customText,
+  })  : assert(_numberOfCharacters > 0,
+            'numberOfCharacters must be greater than 0'),
+        _customText = customText;
+
+  @override
+  String get name =>
+      _customText?.replaceAll('{1}', _numberOfCharacters.toString()) ??
+      'Min of $_numberOfCharacters characters';
+
+  @override
+  bool validate(String value) {
+    return value.length >= _numberOfCharacters;
+  }
+}
+
+/// Validates that the value has at least one special character
+class SpecialCharacterValidationRule extends ValidationRule {
+  final String? _customText;
+
+  SpecialCharacterValidationRule({
+    String? customText,
+  }) : _customText = customText;
+
+  @override
+  String get name => _customText ?? 'Has special character';
+
+  @override
+  bool validate(String value) {
+    return value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+  }
+}
+
+/// Validates that the value has at least one uppercase letter
+class UppercaseValidationRule extends ValidationRule {
+  final String? _customText;
+
+  UppercaseValidationRule({
+    String? customText,
+  }) : _customText = customText;
+
+  @override
+  String get name => _customText ?? 'Has uppercase letter';
+
+  @override
+  bool validate(String value) {
+    return value.contains(RegExp(r'[A-Z]'));
+  }
+}
+
+/// Base class representing a validation rule
+/// Every new validation should extends this one.
+///
+abstract class ValidationRule {
+  /// Name of the rule. You can use this atribute to
+  /// show the name of the rule to the user, for example.
+  String get name;
+
+  /// A function that should be implemented and it represents
+  /// the validation of the value. It should return [Boolean] indicating
+  /// that the value passed pass the validation or not.
+  bool validate(String value);
 }
